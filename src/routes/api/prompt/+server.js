8d3a8c55871ef714a6047073e829ae8f0a4fc59d
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-04-23 20:58:45 +0530

    final final attempt at fixing the cors issue

--- src/routes/api/prompt/+server.js
+++ src/routes/api/prompt/+server.js
@@ -8,16 +8,17 @@ const apiCall = (/** @type {any} */ data, option) => {
   if (option == "DS-lab") {
     result = fetch("https://dashboard.scale.com/spellbook/api/v2/deploy/pu03fnp", {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic clvccda0c00j11au3478b5wk4',
-        'mode': 'no-cors'
       },
       body: JSON.stringify(data1)
     });
   } else if(option == "python-lab") {
     result = fetch("https://dashboard.scale.com/spellbook/api/v2/deploy/5p13f1w", {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic clvccda0c00j11au3478b5wk4',
@@ -28,6 +29,7 @@ const apiCall = (/** @type {any} */ data, option) => {
   } else {
     result = fetch("https://dashboard.scale.com/spellbook/api/v2/deploy/ef23fic", {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic clvccda0c00j11au3478b5wk4',
